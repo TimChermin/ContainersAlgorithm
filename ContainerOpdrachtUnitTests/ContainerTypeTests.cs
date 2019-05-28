6bacab500b2bcccae5e800ea6e6be8b9488da2e7
Tim Chermin <tim.chermin@student.fontys.nl>
2019-05-28 23:05:30 +0200

    moved location for cooling tests

--- ContainerOpdrachtUnitTests/ContainerTypeTests.cs
+++ ContainerOpdrachtUnitTests/ContainerTypeTests.cs
@@ -7,28 +7,7 @@ namespace UnitTests
 {
     public class ContainerTypeTests
     {
-        [Fact]
-        public void Should_ReturnTrue_When_TheContainerHasCoolingAndIfItWillBePlacedInFront()
-        {
-            //Arrange
-            Container containerCool = new Container(4, false, true);
-            Container container = new Container(4, false, false);
-            ContainerRow row = new ContainerRow(5, 5, 5);
-
-            //Act
-            bool resultCoolInFront = row.IfTheContainerHasCoolingOrValuablesIsItInfrontOrInTheBack(containerCool, 1, 5);
-            bool resultCoolNotInFront = row.IfTheContainerHasCoolingOrValuablesIsItInfrontOrInTheBack(containerCool, 2, 5);
-
-            bool resultContainer = row.IfTheContainerHasCoolingOrValuablesIsItInfrontOrInTheBack(container, 1, 5);
-            bool resultContainer2 = row.IfTheContainerHasCoolingOrValuablesIsItInfrontOrInTheBack(container, 2, 5);
-
-            //Assert
-            Assert.True(resultCoolInFront);
-            Assert.False(resultCoolNotInFront);
-
-            Assert.True(resultContainer);
-            Assert.True(resultContainer2);
-        }
+        
 
         [Fact]
         public void Should_ReturnTrue_When_TheContainerHasValuablesAndIfItWillBePlacedInFrontOrInTheBack()
