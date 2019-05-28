d2bf115adb2ec7dff1dc07a155db29c607fe1f1d
Tim Chermin <tim.chermin@student.fontys.nl>
2019-05-29 00:13:01 +0200

    added ship, row and column tests

---
+++ ContainerOpdrachtUnitTests/ColumnTests.cs
@@ -0,0 +1,79 @@
+﻿using ContainerOpdrachtVersion3;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ColumnTests
+    {
+        ContainerColumn column = new ContainerColumn(5, 5, 5);
+
+        [Fact]
+        public void Should_ReturnTrue_When_TryingToPlaceCoolingContainers()
+        {
+            //Arrange
+            Container containerCooling = new Container(4, false, true);
+
+            //Act
+            bool Placed = column.TryToPlaceContainer(containerCooling);
+
+            //Assert
+            Assert.True(Placed);
+        }
+
+        [Fact]
+        public void Should_ReturnTrue_When_TryingToPlaceValualbeContainers()
+        {
+            //Arrange
+            Container containerValuable = new Container(4, true, false);
+
+            //Act
+            bool Placed = column.TryToPlaceContainer(containerValuable);
+
+            //Assert
+            Assert.True(Placed);
+        }
+
+        [Fact]
+        public void Should_ReturnTrue_When_TryingToPlaceNormalContainers()
+        {
+            //Arrange
+            Container container = new Container(4, false, false);
+
+            //Act
+            bool Placed = column.TryToPlaceContainer(container);
+
+            //Assert
+            Assert.True(Placed);
+        }
+
+        [Fact]
+        public void Should_ReturnTrue_When_TryingToPlaceCoolingValuableContainers()
+        {
+            //Arrange
+            Container containerCoolVal = new Container(4, true, true);
+
+            //Act
+            bool Placed = column.TryToPlaceContainer(containerCoolVal);
+
+            //Assert
+            Assert.True(Placed);
+        }
+
+        [Fact]
+        public void Should_ReturnFalse_When_TryingToPlaceOnAFullColumn()
+        {
+            //Arrange
+            Container container = new Container(4, false, false);
+            ContainerRow smallerColumn = new ContainerRow(1, 1, 1);
+            smallerColumn.TryToPlaceContainer(container);
+
+            //Act
+            bool Placed = smallerColumn.TryToPlaceContainer(container);
+
+            //Assert
+            Assert.False(Placed);
+        }
+    }
+}
--- ContainerOpdrachtUnitTests/ShipTests.cs
+++ ContainerOpdrachtUnitTests/ShipTests.cs
@@ -23,5 +23,71 @@ namespace UnitTests
             //Assert
             Assert.True(containerCount == 200);
         }
+
+
+        [Fact]
+        public void Should_PlaceContainer_When_placeingCoolingContainerOnEmptyShip()
+        {
+            //Arrange
+            Ship ship = new Ship(5, 5, 5, 1000);
+            ship.AddContainer(20, false, true);
+            ship.SortListContainersNotOnShip(ship.ContainersLookingForLocation);
+
+            //Act
+            ship.LookForLocationPerContainer();
+            bool coolingPlaced = (ship.ContainerRows[0].ContainerColumns[0].containerStack[0].Cooling == true);
+
+            //Assert
+            Assert.True(coolingPlaced);
+        }
+
+        [Fact]
+        public void Should_PlaceContainer_When_placeingValuableContainerOnEmptyShip()
+        {
+            //Arrange
+            Ship ship = new Ship(5, 5, 5, 1000);
+            ship.AddContainer(20, true, false);
+            ship.SortListContainersNotOnShip(ship.ContainersLookingForLocation);
+
+            //Act
+            ship.LookForLocationPerContainer();
+            bool valuablePlaced = (ship.ContainerRows[0].ContainerColumns[0].containerStack[0].Valuable == true);
+
+            //Assert
+            Assert.True(valuablePlaced);
+        }
+
+        [Fact]
+        public void Should_PlaceContainer_When_placeingContainerOnEmptyShip()
+        {
+            //Arrange
+            Ship ship = new Ship(5, 5, 5, 1000);
+            ship.AddContainer(20, false, false);
+            ship.SortListContainersNotOnShip(ship.ContainersLookingForLocation);
+
+            //Act
+            ship.LookForLocationPerContainer();
+            bool valuablePlaced = (ship.ContainerRows[0].ContainerColumns[0].containerStack[0].Valuable == false);
+
+            //Assert
+            Assert.True(valuablePlaced);
+        }
+
+        [Fact]
+        public void Should_PlaceContainer_When_placeingContainerOnFullShip()
+        {
+            //Arrange
+            Ship ship = new Ship(1, 1, 1, 1000);
+            ship.AddContainer(10, false, false);
+            ship.AddContainer(6, false, false);
+            ship.SortListContainersNotOnShip(ship.ContainersLookingForLocation);
+
+            //Act
+            ship.LookForLocationPerContainer();
+            bool NotPlaced = (ship.ContainersCouldntAddToShip[0].Weight == 6);
+
+            //Assert
+            Assert.True(NotPlaced);
+        }
     }
 }
